5861f1739a8c416e54b496a8e7a3139c734a4ce5
Denis Zavershinskiy <zaverden@gmail.com>
2016-08-24 09:14:47 +0700

    add external config transform for console app

---
+++ ConsoleApplicationSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+using ClassLibrarySample.Properties;
+
+namespace ConsoleApplicationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString);
+            Console.WriteLine(Settings.Default.LibS1);
+            Console.ReadLine();
+        }
+    }
+}
